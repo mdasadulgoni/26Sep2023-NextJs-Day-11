df31a89aeaed2e804cda50a180e393aaec7c573d
mdasadulgoni <mdasadulgoni@gmail.com>
2023-09-26 10:41:15 +0530

    new

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Helo Every One How are You</h1>
+    </>
+  )
+}
